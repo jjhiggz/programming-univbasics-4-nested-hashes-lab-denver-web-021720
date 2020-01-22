b6f8111307052d307973446814b87dca815ef8ca
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-22 10:03:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return Array.Join(programmer_hash[:dennis_ritchie][:languages])
+		 return Array.join(programmer_hash[:dennis_ritchie][:languages])
 
 end
 
